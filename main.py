9a8a130285848f765f79e1421295fd1d0b67a3bb
Alexey-Kobzev <alex20vek1998@gmail.com>
2024-12-19 02:23:36 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+Name_of_the_course = "Python"
+print(Name_of_the_course)
+Completed_homeworks_count = 12
+print(Completed_homeworks_count)
+Hours_for_homeworks_count = 1.5
+print(Hours_for_homeworks_count)
+Time_on_one_task = Hours_for_homeworks_count / Completed_homeworks_count
+print(Time_on_one_task)
+print('Курс:', Name_of_the_course,',', 'всего задач:', Completed_homeworks_count,',', 'затрачено часов:', Hours_for_homeworks_count,',', 'среднее время выполнения', Time_on_one_task, 'часа''.')
